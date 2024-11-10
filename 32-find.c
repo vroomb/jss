d0a613adffb6f6dabae1d0f7d07cc61753fe1eb8
vroomb <159781678+vroomb@users.noreply.github.com>
2024-11-10 23:33:12 +0530

    added 32

---
+++ 32-find.c
@@ -0,0 +1,62 @@
+#include <complex.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void shiftstring(char *str) {
+    for (int i = 0; str[i+1]; i++) {
+        str[i] = str[i+1];
+    }
+}
+
+int ffindword(char *word, FILE *input) {
+    int l_word = strlen(word);
+    char a; int count = 0;
+    a = getc(input);
+    while (a != EOF) {
+        int i = 0;
+        while ((a == word[i]) && (i < l_word)) {
+            i++; a = getc(input);
+        }
+        if (i == l_word) {
+            count++;
+        }
+        a = getc(input);
+    }
+    return count;
+}
+
+int main(int argc, char **argv) {
+    char Help[] = "Findme.exe\n\nSYNTAX:\n    Findme.exe [file] [word]\n\nNOTES:\n    To include spaces in [file] or [word], enclose the respective in \"\" quotes.";
+
+    // if (argc != 3) {
+    //     printf("%s", Help);
+    //     return 0;
+    // }
+    
+    // if (argv[0][0] == '\"') {
+    //     for (int i = 0; i < argc; i++) {
+    //         for (int j = 0; argv[i][j]; j++) {
+    //             if (argv[i][j] == '\"') {
+    //                 strcat(arg, const char *restrict Source)
+    //             }
+    //         }
+    //     }
+    // }
+    // todo: add a way to parse ""
+
+    FILE *input = fopen(argv[1], "r");
+    if (!input){
+        printf("ERR: File not opened, probably does not exist.");
+        return EXIT_FAILURE;
+    }
+
+    // char a = 0;
+    // while (a != EOF) {
+    //     a = getc(input);
+    //     printf("%c", a);
+    // }
+
+    printf("%d", ffindword(argv[2], input));
+    return EXIT_SUCCESS;
+}
